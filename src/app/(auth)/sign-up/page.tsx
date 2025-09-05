26e242100d6f112a4a27d0af479f815d6c70ea1b
Mukul Tiwari <mukulntiwari@gmail.com>
2025-09-05 18:30:36 +0530

    authentication-ui

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,9 @@
+import { SignUpView } from "@/modules/auth/ui/views/sign-up-view";
+
+const Page = () => {
+  console.log("Sign Up Page");
+  return (
+    <SignUpView />
+  );
+}
+export default Page;
