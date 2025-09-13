c9cf03e0a8883a2b728034d5c19cdd9804c6c0a2
Mukul Tiwari <mukulntiwari@gmail.com>
2025-09-13 13:10:28 +0530

    agents-data-table

--- src/modules/agents/ui/views/agent-view.tsx
+++ src/modules/agents/ui/views/agent-view.tsx
@@ -4,6 +4,11 @@ import { ErrorState } from "@/components/error-state";
 import { LoadingState } from "@/components/loading-state";
 import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
+import { DataTable } from "../components/data-table";
+import { columns } from "../components/columns";
+import { EmptyState } from "@/components/empty-state";
+
+
 
 export const AgentsView = () => {
     const trpc = useTRPC();
@@ -11,8 +16,14 @@ export const AgentsView = () => {
 
     
     return (
-        <div>
-            {JSON.stringify(data, null ,2)}
+        <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
+            <DataTable data={data} columns={columns}/>
+            {data.length === 0 && (
+                <EmptyState 
+                    title="Create Your First Agent!!"
+                    description="Create an agent to join your meetings. Each agent will follow your instructions and can also interact with participants during the call."
+                />
+            )}
         </div>
     );
 };
