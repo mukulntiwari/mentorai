0d834f2a59762446f8b79bbeff36c1b6eb3383bc
Mukul Tiwari <mukulntiwari@gmail.com>
2025-09-16 20:57:07 +0530

    payments

--- src/modules/meetings/ui/components/meeting-form.tsx
+++ src/modules/meetings/ui/components/meeting-form.tsx
@@ -13,6 +13,7 @@ import { useState } from "react";
 import { CommandSelect } from "@/components/command-select";
 import { GeneratedAvatar } from "@/components/generated-avatar";
 import { NewAgentDialog } from "@/modules/agents/ui/components/new-agent-dialog";
+import { useRouter } from "next/navigation";
 
 interface MeetingFormProps {
     onSuccess?: (id?: string) => void;
@@ -27,6 +28,7 @@ export const MeetingForm = ({
 }: MeetingFormProps) => {
     const trpc = useTRPC();
     const queryClient = useQueryClient();
+    const router = useRouter();
 
     const [openNewAgentDialog, setOpenNewAgentDialog] = useState(false);
     const [agentSearch, setAgentSearch] = useState("");
@@ -44,11 +46,18 @@ export const MeetingForm = ({
                 await queryClient.invalidateQueries(
                     trpc.meetings.getMany.queryOptions({}),
                 );
+                await queryClient.invalidateQueries(
+                    trpc.premium.getFreeUsage.queryOptions(),
+                );
 
                 onSuccess?.(data.id);
             },
             onError: (error) => {
-                toast.error(error.message)
+                toast.error(error.message);
+
+            if(error.data?.code === "FORBIDDEN") {
+                router.push("/upgrade");
+            }
             },
         }),
     );
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,11 +1,13 @@
 import { agentsRouter } from '@/modules/agents/server/procedures';
 import { createTRPCRouter } from '../init';
 import { MeetingsRouter } from '@/modules/meetings/server/procedure';
+import { premiumRouter } from '@/modules/premium/server/procedures';
 
 
 export const appRouter = createTRPCRouter({
   agents: agentsRouter,
   meetings: MeetingsRouter,
+  premium: premiumRouter,
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
